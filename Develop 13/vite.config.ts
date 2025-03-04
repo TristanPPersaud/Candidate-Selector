a22fbaaf00a1bf6fa63e410291041e17726401d6
Tristan Persaud <tristanppersaud@gmail.com>
2025-03-04 09:25:46 -0600

    vite fix for render

--- Develop 13/vite.config.ts
+++ Develop 13/vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
     host: "0.0.0.0", // Allows external access
     port: Number(process.env.PORT) || 3000, // Convert PORT to a number
     strictPort: true, // Prevents port conflicts
+    allowedHosts: ["candidate-selector-3.onrender.com"], 
   },
 });
